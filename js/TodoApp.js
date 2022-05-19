9391b1950c2cb60cdc395a4f26a2882b5ef38f23
AbhishekB27 <akstyles27@gmail.com>
2022-05-19 22:56:35 +0530

    only logic part is left of ToDo-App

---
+++ js/TodoApp.js
@@ -0,0 +1,7 @@
+console.log("TODO-APP");
+const addButton = document.querySelector('.add-button');
+const todoInput = document.querySelector('.todo-input');
+console.log(todoInput);
+addButton.addEventListener('click',()=>{
+    // todoInput.classList
+})
