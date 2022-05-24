7c8eb043a23bac8ac51021c308bcc487a008ddd7
AbhishekB27 <akstyles27@gmail.com>
2022-05-24 11:58:34 +0530

    after long research i have completed the application almost

--- js/StonePaperScissor.js
+++ js/StonePaperScissor.js
@@ -19,6 +19,9 @@ var scissors = getRefOfElement('.scissors');
 var sto = getRefOfElement('.hFistI');
 var pap = getRefOfElement('.hPaperI');
 var sci = getRefOfElement('.hScissorI');
+var stoClone = sto.cloneNode(true)
+var papClone= pap.cloneNode(true)
+var sciClone = sci.cloneNode(true)
 var gameTimer= getRefOfElement('.timer');
 var game_container = getRefOfElement('.game');
 var winTitle = getRefOfElement('.win-title');
@@ -28,6 +31,8 @@ var ruleClose = getRefOfElement('.close-button');
 var userClick = getRefOfElement('.userClick');
 var botClick = getRefOfElement('.botClick');
 var startGame = getRefOfElement('.start-game');
+var uScore = getRefOfElement('.score-c2');
+var playAgain = getRefOfElement('.ply-again');
 var game = ["stone","paper","scissor"];
 var time=0;
 var uPicked='';
@@ -56,27 +61,41 @@ ruleButton.addEventListener('click',()=>{
 ruleClose.addEventListener('click',()=>{
     aboutRules.style='display:none';
 });
+var uS = null;
+var bS = null;
+const getPreviouState = (u,b)=>{
+    uS=u;
+    bS=b;
+}
 //when a match have drawn
 const draw =(user,bot)=>{
+    getPreviouState(user.move,bot.move);
     game_container.style = 'display:none;';
     uiResult.style = 'display:flex';
     winTitle.innerHTML=`Match ${user.status}`;
     console.log(user.status)
     botClick.classList.add(`${user.move}`);
     userClick.classList.add(`${bot.move}`)
-    let cloneNode= user.icon.cloneNode(true);
-    console.log(cloneNode);
-    userClick.appendChild(cloneNode);
+    // let uCloneNode= user.icon.cloneNode(true);
+    // let bCloneNote = bot.icon.cloneNode(true);
+    // console.log(cloneNode);
+    // userClick.appendChild();
+    // botClick.appendChild();
+    console.log(user.icon)
+    userClick.appendChild(user.icon.cloneNode(true));
     botClick.appendChild(bot.icon);
-    console.log(bot.icon)
+    // console.log(bot.icon)
 }
 //Result
 const result = (winner,looser)=>{
     alert("Winner: " + winner.name )
     game_container.style = 'display:none;';
     uiResult.style = 'display:flex';
+    // let winnerCloneNode= winner.icon.cloneNode(true);
+    // let looserCloneNode = looser.icon.cloneNode(true);
     
     if(winner.name === 'Bot' && (winner.move === 'stone' || winner.move === 'paper' ||winner.move === 'scissors')){
+        getPreviouState(`${looser.move}`,`${winner.move}`);
         winTitle.innerHTML=`${winner.name} Win`;
         console.log(winner.name + ":" + winner.move)
         botClick.classList.add(`${winner.move}`);
@@ -85,6 +104,10 @@ const result = (winner,looser)=>{
         botClick.appendChild(winner.icon);
         userClick.appendChild(looser.icon);
     }else if(winner.name === 'User' && (winner.move === 'stone' || winner.move === 'paper' ||winner.move === 'scissors')){
+        score++;
+        getPreviouState(winner.move,looser.move);
+        score > 9 ? uScore.innerHTML= `${score}`:uScore.innerHTML= `0${score}`
+        uScore.innerHTML= `0${score}`
         winTitle.innerHTML=`${winner.name} Win`;
         console.log(winner.name + ":" + winner.move)
         userClick.classList.add(`${winner.move}`)
@@ -117,17 +140,17 @@ stone.addEventListener('click',()=>{
     botPicked=bot();
     if(uPicked === botPicked){
         alert("Match Tie");
-        draw({name:'User',icon:sto,move:'stone',status:'Tie'},{name:'Bot',icon:sto,move:'stone',status:'Tie'});
+        draw({name:'User',icon:stoClone,move:'stone',status:'Tie'},{name:'Bot',icon:stoClone,move:'stone',status:'Tie'});
     }
     switch(botPicked){
         case 'paper':{
             console.log("Bot win: "+"Paper beat stone");
-            result({name:'Bot',icon:pap,move:'paper'},{name:'User',icon:sto,move:'stone'});
+            result({name:'Bot',icon:papClone,move:'paper'},{name:'User',icon:stoClone,move:'stone'});
         }
         break;
         case 'scissor':{
             console.log("User win: "+"Stone beat Scissor");
-            result({name:'User',icon:sto,move:'stone'},{name:'Bot',icon:sci,move:'scissors'});
+            result({name:'User',icon:stoClone,move:'stone'},{name:'Bot',icon:sciClone,move:'scissors'});
         }
         break;
     }
@@ -138,17 +161,17 @@ paper.addEventListener('click',()=>{
     botPicked=bot();
     if(uPicked === botPicked){
         alert("Match Tie");
-        draw({name:'User',icon:pap,move:'paper',status:'Tie'},{name:'Bot',icon:pap,move:'paper',status:'Tie'});
+        draw({name:'User',icon:papClone,move:'paper',status:'Tie'},{name:'Bot',icon:papClone,move:'paper',status:'Tie'});
     }
     switch(botPicked){
         case 'stone':{
             console.log("User win2: "+"Paper beat stone");
-            result({name:'User',icon:pap,move:'paper'},{name:'Bot',icon:sto,move:'stone'});
+            result({name:'User',icon:papClone,move:'paper'},{name:'Bot',icon:stoClone,move:'stone'});
         }
         break;
         case 'scissor':{
             console.log("Bot win2: "+"Scissor beat Paper");
-            result({name:'Bot',icon:sci,move:'scissors'},{name:'User',icon:pap,move:'paper'});
+            result({name:'Bot',icon:sciClone,move:'scissors'},{name:'User',icon:papClone,move:'paper'});
         }
         break;
     }
@@ -159,19 +182,32 @@ scissors.addEventListener('click',()=>{
     botPicked=bot();
     if(uPicked === botPicked){
         alert("Match Tie");
-        draw({name:'User',icon:sci,move:'scissors',status:'Tie'},{name:'Bot',icon:sci,move:'scissors',status:'Tie'});
+        draw({name:'User',icon:sciClone,move:'scissors',status:'Tie'},{name:'Bot',icon:sciClone,move:'scissors',status:'Tie'});
     }
     switch(botPicked){
         case 'stone':{
             console.log("Bot win3: "+"Stone beat scissor");
-            result({name:'Bot',icon:sto,move:'stone'},{name:'User',icon:sci,move:'scissors'});
+            result({name:'Bot',icon:stoClone,move:'stone'},{name:'User',icon:sciClone,move:'scissors'});
         }
         break;
         case 'paper':{
             console.log("User win3: "+"Scissor beat paper");
-            result({name:'User',icon:sci,move:'scissors'},{name:'Bot',icon:pap,move:'paper'});
+            result({name:'User',icon:sciClone,move:'scissors'},{name:'Bot',icon:papClone,move:'paper'});
         }
         break;
     }
 })
+//after win or lose play again functionality
+playAgain.addEventListener('click',()=>{
+    time=00;
+    botClick.innerHTML=""
+    userClick.innerHTML=""
+    userClick.classList.remove(`${uS}`);
+    botClick.classList.remove(`${bS}`);
+    botClick.classList.remove('anim')
+    userClick.classList.remove('anim')
+    game_container.style = 'display:flex;';
+    uiResult.style = 'display:none';
+    console.log(move)
+});
 }
