bb8454d4611026904ca87b0179f3f1c5659d828b
AbhishekB27 <akstyles27@gmail.com>
2023-05-04 22:42:00 +0530

    changed

--- js/Countdown.js
+++ js/Countdown.js
@@ -1,37 +1,80 @@
-console.log("COUNTDOWN")
-var cdDay= document.getElementById('day');
-var cdHour= document.getElementById('hour');
-var cdMinute= document.getElementById('minute');
-var cdSecond= document.getElementById('second');
-var countdown = document.getElementById('countdown');
-var spinner = document.getElementById('spinner');
+console.log("COUNTDOWN");
+var cdDay = document.getElementById("day");
+var cdHour = document.getElementById("hour");
+var cdMinute = document.getElementById("minute");
+var cdSecond = document.getElementById("second");
+var countdown = document.getElementById("countdown");
+var spinner = document.getElementById("spinner");
+var setting = document.getElementById("setting");
+var dateInput = document.getElementById("dateInput");
+var dateContainer = document.getElementById("dateContainer");
+const enterButton = document.getElementById("enterButton");
+let endingDate = "";
 
-
-
-setInterval(() => {
-    const time1 = new Date("jun/12/2022 12:00:00").getTime();
-    const time2 = new Date().getTime();
-    const difference = time1- time2;
-    console.log(difference)
-    const day= Math.floor( difference / (1000 * 60 * 60 * 24) );
-    const hours =Math.floor( difference % (1000 * 60 * 60 * 24)/(1000* 60 * 60) ) // ms in 1 hr = 1000*60*60
-    const min = Math.ceil( difference % (1000 * 60 * 60 )/(1000* 60))// ms in1 min = 1000*60
-    const sec = Math.ceil( difference % (1000 * 60 )/(1000) )// ms in 1sec = 1000
-    console.log("Days: " + day)
-    console.log("Hours: "+hours)
-    console.log("Minutes: "+min)
-    console.log("Seconds: "+sec)
-    // countdown.innerHTML =`${day} : ${hours} : ${min} : ${sec}`
-    setTime(day,hours,min,sec)
-},1000);
-
-const setTime = (day,hours,min,sec)=>{
-        day < 10 ? (cdDay.innerHTML=`0${day}`) : (cdDay.innerHTML=`${day}`);
-        hours < 10 ? (cdHour.innerHTML=`0${hours}`) : (cdHour.innerHTML=`${hours}`);
-        min < 10 ? (cdMinute.innerHTML=`0${min}`) : (cdMinute.innerHTML=`${min}`);
-        sec < 10 ? (cdSecond.innerHTML=`0${sec}`) : (cdSecond.innerHTML=`${sec}`);
-
-
+let existDate = new Date(JSON.parse(localStorage.getItem("date")));
+console.log(new Date(JSON.parse(localStorage.getItem("date"))).toLocaleDateString())
+if (existDate) {
+  console.log("Date already exists");
+  dateContainer.style.display = "none";
+  endingDate = existDate.toLocaleDateString();
+  setCountDown();
+  dateInput.value = `${existDate.getFullYear()}-${existDate.getMonth()+1}-0${existDate.getDate()}`
+} else {
+  console.log("Date not exists");
 }
+console.log(dateInput);
+setting.addEventListener("click", () => {
+  dateContainer.style.display = "flex";
+});
+// console.log("Good: " + new Date(`2023-11-20`).getFullYear())
 
+enterButton.addEventListener("click", () => {
+  const dateRegex = /^\d{4}-\d{2}-\d{2}$/;
+  if (
+    dateInput.value.match(dateRegex) &&
+    new Date(`${dateInput.value}`).getTime() > new Date().getTime()
+  ) {
+    console.log("Good: " + new Date(`${dateInput.value}`));
+    endingDate = `${new Date(`${dateInput.value}`).toLocaleDateString()}`;
+    localStorage.setItem(
+      "date",
+      JSON.stringify(new Date(`${dateInput.value}`).toLocaleDateString())
+    );
+    dateContainer.style.display = "none";
+    setCountDown();
+  } else {
+    alert("Enter Again");
+  }
+});
 
+console.log(endingDate);
+console.log(new Date().getMonth())
+function remainingTime() {
+  const time1 = new Date(`${endingDate}`).getTime();
+  const time2 = Date.parse(new Date());
+  const total = time1 - time2;
+  const seconds = Math.floor((total / 1000) % 60);
+  const minutes = Math.floor((total / 1000 / 60) % 60);
+  const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+  const days = Math.floor(total / (1000 * 60 * 60 * 24));
+  return {
+    total,
+    days,
+    hours,
+    minutes,
+    seconds,
+  };
+}
+function setCountDown() {
+  const timeInter = setInterval(() => {
+    const time = remainingTime();
+    cdDay.innerHTML = `${time.days}`;
+    cdHour.innerHTML = `${time.hours}`;
+    cdMinute.innerHTML = `${time.minutes}`;
+    cdSecond.innerHTML = `${time.seconds}`;
+    if (time.total <= 0) {
+      clearInterval(timeInter);
+      // alert("Time Over")
+    }
+  }, 1000);
+}
